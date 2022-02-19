026e9dcebf3b8a0a1c8cdf957913e1e0bdc80a6e
tmigot <tangi.migot@gmail.com>
2022-02-19 04:24:31 +0100

    fix doc build

--- docs/make.jl
+++ docs/make.jl
@@ -36,7 +36,7 @@ makedocs(
   modules = [FletcherPenaltyNLPSolver],
   doctest = true,
   # linkcheck = true,
-  strict = true,
+  # strict = true,
   format = Documenter.HTML(
     assets = ["assets/style.css"],
     prettyurls = get(ENV, "CI", nothing) == "true",
