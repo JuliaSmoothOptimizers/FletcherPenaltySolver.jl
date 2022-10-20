df9cb3b16ab7aa9e0b070356063eef9d14e2c821
tmigot <tangi.migot@gmail.com>
2022-10-20 00:07:26 -0400

    add `restart` from a different problem

--- test/restart.jl
+++ test/restart.jl
@@ -13,7 +13,6 @@
 
   stats = GenericExecutionStats(nlp)
   solver = FPSSSolver(nlp)
-  stp = solver.stp
   SolverCore.solve!(solver, nlp, stats)
   @test stats.status_reliable && stats.status == :first_order
   @test stats.solution_reliable && isapprox(stats.solution, [1.0; 1.0], atol = 1e-6)
@@ -24,4 +23,43 @@
   SolverCore.solve!(solver, nlp, stats, atol = 1e-10, rtol = 1e-10)
   @test stats.status_reliable && stats.status == :first_order
   @test stats.solution_reliable && isapprox(stats.solution, [1.0; 1.0], atol = 1e-6)
+end
+
+@testset "Test restart with a different problem" begin
+  nlp = ADNLPModels.ADNLPModel(
+    x -> 0.0,
+    [-1.2; 1.0],
+    [1],
+    [1],
+    [-1.0],
+    x -> [10 * (x[2] - x[1]^2)],
+    [-1.0, 0.0],
+    [-1.0, 0.0],
+    name = "mgh01feas";
+  )
+
+  stats = GenericExecutionStats(nlp)
+  solver = FPSSSolver(nlp)
+  stats = SolverCore.solve!(solver, nlp, stats)
+  @test stats.status_reliable && stats.status == :first_order
+  @test stats.solution_reliable && isapprox(stats.solution, [1.0; 1.0], atol = 1e-6)
+
+  f2(x) = (x[1])^2 + 4 * (x[2] - x[1]^2)^2
+  nlp = ADNLPModel(f2, [-1.2; 1.0])
+  nlp = ADNLPModels.ADNLPModel(
+    x -> 0.0,
+    [-1.2; 1.0],
+    [1],
+    [1],
+    [-1.0],
+    x -> [10 * (x[2] - x[1]^2)],
+    [0.0, 0.0],
+    [0.0, 0.0],
+    name = "mgh01feas";
+  )
+  SolverCore.reset!(solver, nlp)
+
+  stats = SolverCore.solve!(solver, nlp, stats, atol = 1e-10, rtol = 1e-10)
+  @test stats.status_reliable && stats.status == :first_order
+  @test stats.solution_reliable && isapprox(stats.solution, [0.0; 0.0], atol = 1e-6)
 end
